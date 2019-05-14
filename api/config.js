6bda6fb3f5f0223490a5934e2fc819c1069ff3a4
vupebox <45156746+vupebox@users.noreply.github.com>
2019-05-14 14:48:22 -0600

    cambio de conexion mongo

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var config = {
-  mongo: 'mongodb+srv://Julian:Vupebox123456@vupebox-3xw5o.mongodb.net/test?retryWrites=true',
+  mongo: 'mongodb+srv://Alex:bvpmwf35@vupebox-3xw5o.mongodb.net/test?retryWrites=true',
   jwtPrivateKey: 'changethiskey',
   adminFolder: '../admin',
   staticFolder: './static'
